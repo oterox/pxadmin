7f6053aa4f3fcf8b155d865964c3675b944af0e2
oterox <oterox@gmail.com>
2013-05-01 17:26:14 +0200

    created form type for tasks

--- src/Pixellary/TaskBundle/Controller/DefaultController.php
+++ src/Pixellary/TaskBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Pixellary\TaskBundle\Entity\Task;
+use Pixellary\TaskBundle\Form\Type\TaskType;
 
 class DefaultController extends Controller
 {
@@ -30,13 +31,7 @@ class DefaultController extends Controller
         $task = new Task();
         $task->setTaskDeadline(new \DateTime('first day of next month'));
 
-        $form = $this->createFormBuilder($task)
-            ->add('taskCode')
-            ->add('taskName')
-            ->add('taskDesc', 'textarea')
-            ->add('taskDeadline', 'date')
-            ->add('taskProgress', 'integer')
-            ->getForm();
+        $form = $this->createForm(new TaskType(), $task);
 
         if ($request->getMethod() == 'POST') {
 
--- src/Pixellary/TaskBundle/Form/Type/TaskType.php
+++ src/Pixellary/TaskBundle/Form/Type/TaskType.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace Pixellary\TaskBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('taskCode')
+                ->add('taskName')
+                ->add('taskDesc', 'textarea')
+                ->add('taskDeadline', 'date')
+                ->add('taskProgress', 'percent', array('type' => 'integer' ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'task';
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'Pixellary\TaskBundle\Entity\Task',
+        );
+    }
+
+}
+
