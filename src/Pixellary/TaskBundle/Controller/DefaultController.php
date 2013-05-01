d38f260c1d031fff9069f1e460447aca15950520
oterox <oterox@gmail.com>
2013-05-01 14:24:35 +0200

    adding task forms

--- src/Pixellary/TaskBundle/Controller/DefaultController.php
+++ src/Pixellary/TaskBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Pixellary\TaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Pixellary\TaskBundle\Entity\Task;
 
 class DefaultController extends Controller
 {
@@ -18,12 +19,28 @@ class DefaultController extends Controller
 
         $tasks = $em->getRepository('PXTasksBundle:Task')->findAll();
 
-        return $this->render('PXTasksBundle::taskInsert.html.twig', array( 'tasks'=> $tasks ));
+        return $this->render('PXTasksBundle::taskList.html.twig', array( 'tasks'=> $tasks ));
     }
 
     public function taskFormAction()
     {
-        return $this->render('PXTasksBundle::taskForm.html.twig', array());
+        //$request = $this->getRequest();
+
+        $task = new Task();
+        $task->setTaskDeadline(new \DateTime('first day of next month'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('taskCode')
+            ->add('taskName')
+            ->add('taskDesc', 'textarea')
+            ->add('taskDeadline', 'date')
+            ->getForm();
+        return $this->render(
+            'PXTasksBundle::taskForm.html.twig',
+            array('form' => $form->createView())
+        );
+
     }
+
 
 }
