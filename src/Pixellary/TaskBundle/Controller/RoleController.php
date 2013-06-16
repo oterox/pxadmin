19acd5bee53bdaab897815cab30cf7b66dad1c83
oterox <oterox@gmail.com>
2013-06-16 20:07:09 +0200

    update

---
+++ src/Pixellary/TaskBundle/Controller/RoleController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Pixellary\TaskBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Pixellary\TaskBundle\Entity\Role;
+use Pixellary\TaskBundle\Form\RoleType;
+
+/**
+ * Role controller.
+ *
+ * @Route("/admin/role")
+ */
+class RoleController extends Controller
+{
+    /**
+     * Lists all Role entities.
+     *
+     * @Route("/", name="admin_role")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('PXTasksBundle:Role')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Role entity.
+     *
+     * @Route("/", name="admin_role_create")
+     * @Method("POST")
+     * @Template("PXTasksBundle:Role:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Role();
+        $form = $this->createForm(new RoleType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_role_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Role entity.
+     *
+     * @Route("/new", name="admin_role_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Role();
+        $form   = $this->createForm(new RoleType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Role entity.
+     *
+     * @Route("/{id}", name="admin_role_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PXTasksBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Role entity.
+     *
+     * @Route("/{id}/edit", name="admin_role_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PXTasksBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $editForm = $this->createForm(new RoleType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Role entity.
+     *
+     * @Route("/{id}", name="admin_role_update")
+     * @Method("PUT")
+     * @Template("PXTasksBundle:Role:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('PXTasksBundle:Role')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Role entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new RoleType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_role_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Role entity.
+     *
+     * @Route("/{id}", name="admin_role_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('PXTasksBundle:Role')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Role entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_role'));
+    }
+
+    /**
+     * Creates a form to delete a Role entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
